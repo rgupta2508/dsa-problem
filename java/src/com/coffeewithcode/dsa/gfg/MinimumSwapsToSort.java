684d2ecf122e20a646598d3e0e06973d5b808405
rgupta2508 <gupta@localhost>
2022-07-21 03:48:12 +0530

    Add Soln for LongestPalindromicSubstring.java

--- java/src/com/coffeewithcode/dsa/gfg/MinimumSwapsToSort.java
+++ java/src/com/coffeewithcode/dsa/gfg/MinimumSwapsToSort.java
@@ -40,7 +40,7 @@ public class MinimumSwapsToSort {
 		return ans;
 	}
 
-	private void swap(int arr[], int i, int j) {
+	public static void swap(int arr[], int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
